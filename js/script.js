054b4e0f031b1a388142c127bd2b4c20524eb6f3
rusakroman <rrusak87@gmail.com>
2023-01-12 14:24:47 +0300

    Сделал несколько страничек по Бэтмену и начал работать со слайдерами

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const sliderThumbs = new Swiper('.slider-thumbs', {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 3,
+});
+
+const sliderMain = new Swiper('.slider-main', {
+  loop: true
+});
